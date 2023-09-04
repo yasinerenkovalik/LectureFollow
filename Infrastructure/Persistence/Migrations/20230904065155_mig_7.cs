0b0cfc95c01b0d7c329e3a3fc9210417138ecae5
Yasin Eren Kovalık <94734316+yasinerenkovalik@users.noreply.github.com>
2023-09-04 11:30:20 +0300

    04.09.2023

---
+++ Infrastructure/Persistence/Migrations/20230904065155_mig_7.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class mig_7 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lectures",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    FacultyId = table.Column<int>(type: "integer", nullable: false),
+                    Semester = table.Column<string>(type: "text", nullable: false),
+                    Credit = table.Column<float>(type: "real", nullable: false),
+                    Active = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lectures", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudentLectures",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StudetId = table.Column<int>(type: "integer", nullable: false),
+                    LectureId = table.Column<int>(type: "integer", nullable: false),
+                    Active = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentLectures", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Lectures");
+
+            migrationBuilder.DropTable(
+                name: "StudentLectures");
+        }
+    }
+}
---
+++ LectureAPI/Controllers/LectureController.cs
@@ -0,0 +1,96 @@
+using Application.Services;
+using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LectureAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class LectureController : ControllerBase
+    {
+        private readonly ILectureService _lectureService;
+
+        public LectureController(ILectureService lectureService)
+        {
+            _lectureService = lectureService;
+        }
+        [HttpPost("add")]
+        public IActionResult Add(Lecture lecture)
+        {
+            var result= _lectureService.Add(lecture);
+            if (result)
+            {
+               
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _lectureService.GetAll();
+            if (result==null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(result);
+        }
+        
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _lectureService.GetById(id);
+            if (result == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(result);
+
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete(int id)
+        {
+            var result=_lectureService.Delete(id);
+            if (result)
+            {
+                return Ok();
+            }
+
+            return BadRequest();
+
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update(Lecture lecture)
+        {
+            var result=_lectureService.Update(lecture);
+            if (result)
+            {
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        [HttpDelete("harddelete")]
+        public IActionResult HardDelete(int id)
+        {
+            var result=_lectureService.HardDelete(id);
+            if (result)
+            {
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+       
+    }
+}
